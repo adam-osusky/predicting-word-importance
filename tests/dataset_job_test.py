92f39b14131305b88e446b79f9df9e2c7ec78266
Adam Osusky <adam.osusky02@gmail.com>
2024-02-23 15:29:12 +0100

    data: test for wiki lm inserting

--- tests/dataset_job_test.py
+++ tests/dataset_job_test.py
@@ -3,15 +3,13 @@ import os
 from pathlib import Path
 
 import pytest
-from datasets import Dataset, load_dataset
-from predwordimp.data.dataset_job import WikiTextDsJob, test_range
+from datasets import Dataset
+from predwordimp.data.dataset_job import WikiTextDsJob, load_wiki_ds, test_range
 
 
 @pytest.fixture
 def sample() -> dict[str, str]:
-    return {
-        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    }
+    return {"text": "Over the years, she learned how to make a neat scarf."}
 
 
 @pytest.fixture
@@ -38,15 +36,22 @@ def wiki_text_ds_job() -> WikiTextDsJob:
 
 
 @pytest.mark.parametrize("insert_rate", [0.0, 0.25, 0.5, 0.75, 1.0])
+@pytest.mark.parametrize("insert_method", ["random", "google-bert/bert-base-uncased"])
 def test_insert_words(
     wiki_text_ds_job: WikiTextDsJob,
     sample: dict[str, str],
     vocab_ds: Dataset,
     insert_rate: float,
+    insert_method: str,
 ) -> None:
     wiki_text_ds_job.insert_rate = insert_rate
+    wiki_text_ds_job.insert_model = insert_method
+    wiki_text_ds_job.full_ds = vocab_ds
 
-    result = wiki_text_ds_job.insert_words(sample, vocab_ds)
+    if insert_method != "random":
+        wiki_text_ds_job.load_lm()
+
+    result = wiki_text_ds_job.insert_words(sample)
 
     assert "words" in result
     assert "target" in result
@@ -69,9 +74,11 @@ def test_insert_words(
     assert without_inserted == sample["text"]
 
 
+@pytest.mark.parametrize("insert_method", ["random", "google-bert/bert-base-uncased"])
 def test_run_wiki_text_ds_job(
     wiki_text_ds_job: WikiTextDsJob,
     tmp_path: Path,
+    insert_method: str,
 ) -> None:
     job_name = "test_run_wiki_text_ds_job"
     wiki_text_ds_job.job_name = job_name
@@ -81,6 +88,7 @@ def test_run_wiki_text_ds_job(
     os.chdir(str(tmp_path))
 
     # Run the job
+    wiki_text_ds_job.insert_model = insert_method
     wiki_text_ds_job.run()
 
     config_file_path = data_dir / "config.json"
@@ -91,15 +99,15 @@ def test_run_wiki_text_ds_job(
         assert jsonl_file_path.is_file()
 
         # load og split to compare sequence of not inserted words
-        ds = load_dataset(
-            path="wikitext", name="wikitext-103-raw-v1", split=splt, streaming=False
-        )  # type: ignore
-        ds = ds.select(test_range)  # type: ignore
-        ds = wiki_text_ds_job.preprocess_dataset(ds)  # type: ignore
+        ds = load_wiki_ds(splt)
+        ds = ds.select(test_range)
+        ds = wiki_text_ds_job.preprocess_dataset(ds)
         texts = set([sample["text"] for sample in ds])  # type: ignore
 
         with open(jsonl_file_path, "r") as jsonl_file:
             lines = jsonl_file.readlines()
+
+            assert len(lines) == len(ds)  # for every sample insertion was completed
 
             for line in lines:
                 example = json.loads(line)
