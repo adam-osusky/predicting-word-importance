74368801cd98fe0368b34f8b760f37e660222f1c
Adam Osusky <adam.osusky02@gmail.com>
2024-04-09 16:17:12 +0200

    eval: getting experiment name - from jsons get insertion method and rate

--- src/predwordimp/eval/util.py
+++ src/predwordimp/eval/util.py
@@ -1,4 +1,6 @@
+import json
 import math
+import os
 
 
 def get_rank_limit(limit: int | float, length: int) -> int:
@@ -6,3 +8,18 @@ def get_rank_limit(limit: int | float, length: int) -> int:
         return limit
     elif isinstance(limit, float):
         return math.ceil(length * limit)
+
+
+def get_model_name(model_path: str, ds_dir: str = "data/metacentrum/wikitext") -> str:
+    experiment_dir = os.path.dirname(model_path)
+
+    with open(os.path.join(experiment_dir, "train_config.json")) as f:
+        train_config = json.load(f)
+        dataset_name = os.path.basename(os.path.normpath(train_config["dataset_dir"]))
+
+    with open(os.path.join(ds_dir, dataset_name, "config.json")) as f:
+        ds_config = json.load(f)
+
+    model = "bert" if "bert" in ds_config["insert_model"] else ds_config["insert_model"]
+
+    return f"{model}-{ds_config['insert_rate']}"
