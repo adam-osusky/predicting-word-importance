96e4a7b3d49a8a4caa8593049b7c2e6dca527fb4
Adam Osusky <adam.osusky02@gmail.com>
2024-04-30 19:00:11 +0200

    util: evaluation models in dir on specified domain

--- src/predwordimp/util/eval_dir.py
+++ src/predwordimp/util/eval_dir.py
@@ -7,11 +7,12 @@ JSON_NAME = "eval.json"
 MAX_RANK_LIMIT = 0.1
 
 
-def generate_json_file(experiment_dir: str, experiment_name: str) -> None:
+def generate_json_file(experiment_dir: str, experiment_name: str, domain: str) -> None:
     data = {
         "job": "EvalWordImp",
         "hf_model": os.path.join(experiment_dir, experiment_name, experiment_name),
         "max_rank_limit": MAX_RANK_LIMIT,
+        "domain": domain,
         "seed": 69,
     }
 
@@ -20,14 +21,18 @@ def generate_json_file(experiment_dir: str, experiment_name: str) -> None:
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python eval_dir.py <dir_with_models>")
+    if len(sys.argv) not in [2, 3]:
+        print("Usage: python eval_dir.py <dir_with_models> [domain]")
         sys.exit(1)
+
+    domain = ""
+    if len(sys.argv) == 3:
+        domain = sys.argv[2]
 
     directory_path = sys.argv[1]
     directories = os.listdir(directory_path)
     for directory in directories:
-        generate_json_file(directory_path, directory)
+        generate_json_file(directory_path, directory, domain)
         subprocess.run(["python", "src/predwordimp/util/run.py", JSON_NAME])
 
     os.remove(JSON_NAME)
