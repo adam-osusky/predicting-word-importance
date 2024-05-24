0b47074f3e6d06d65d08470d15a611dbc3255129
Adam Osusky <adam.osusky02@gmail.com>
2024-05-24 14:58:05 +0200

    kpe: saving of results & docstr

--- src/predwordimp/eval/kpe.py
+++ src/predwordimp/eval/kpe.py
@@ -1,6 +1,7 @@
 import json
+import os
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Literal
 
 import numpy as np
 import torch
@@ -19,10 +20,23 @@ from transformers.tokenization_utils import BatchEncoding
 
 from predwordimp.eval.wi_eval import EvalWordImpTFIDF
 from predwordimp.util.job import ConfigurableJob
+from predwordimp.util.logger import get_logger
+
+logger = get_logger(__name__)
+
+# TODO add creation of json predictions for understanding
 
 
 @dataclass
 class Candidate:
+    """Represents a candidate phrase for keyphrase extraction.
+
+    Attributes:
+        phrase (str): The candidate phrase.
+        start_pos (int): The starting position of the phrase in the original text.
+        end_pos (int): The ending position of the phrase in the original text.
+    """
+
     phrase: str
     start_pos: int
     end_pos: int
@@ -30,13 +44,38 @@ class Candidate:
 
 @dataclass
 class KpeEvalJob(ConfigurableJob):
+    """Job for evaluating keyphrase extraction models. Our models and TF-IDF.
+
+    Attributes:
+        model_names_or_paths (list[str]): A list of HF model names or paths to evaluate.
+        dataset (str): The name of the dataset to use for evaluation.
+        split (str): The split of the dataset to use (default is "test").
+        dataset_subset (str): The subset of the dataset to use (default is "generation").
+            Useful when the HF dataset has extractive and generation keyphrase mode.
+        output_dir_name (str | None): The name of the output directory for results. (default is None).
+    """
+
     model_names_or_paths: list[str]
-    dataset: str
+    dataset: Literal[
+        "taln-ls2n/inspec",
+        "taln-ls2n/semeval-2010-pre",
+        "midas/duc2001",
+        "midas/semeval2017",
+    ]
     split: str = "test"
     dataset_subset: str = "generation"
+    output_dir_name: str | None = None
 
     @staticmethod
     def get_candidate_pos(text: str) -> list[Candidate]:
+        """Extracts candidate phrases from the text using noun phrase grammar.
+
+        Args:
+            text (str): The input text.
+
+        Returns:
+            list[Candidate]: A list of candidate phrases with their positions.
+        """
         noun_phrases = []
         grammar = r"""
     NP: {<JJ>*<NN|NNS|NNP|NNPS>+}  # Adjectives (zero or more) followed by nouns (one or more)
@@ -44,6 +83,7 @@ class KpeEvalJob(ConfigurableJob):
         cp = RegexpParser(grammar)
         char_pos = 0
         words = word_tokenize(text)
+        # words = text.split()
         tagged_words = pos_tag(words)
         tree = cp.parse(tagged_words)
 
@@ -52,7 +92,7 @@ class KpeEvalJob(ConfigurableJob):
                 phrase = " ".join(word for word, pos in subtree.leaves())
                 start_pos = text.find(phrase, char_pos)
                 if start_pos == -1:
-                    print("Phrase not found in the text!")
+                    logger.info(f"Phrase not found in the text! The phrase `{phrase}`")
                     continue
                 end_pos = start_pos + len(phrase)
                 noun_phrases.append(Candidate(phrase, start_pos, end_pos))
@@ -64,6 +104,16 @@ class KpeEvalJob(ConfigurableJob):
     def eval_prf(
         top_N_keyphrases: list[str], references: list[str], cutoff: int = 5
     ) -> tuple[float, float, float]:
+        """Evaluates precision, recall, and F1-score for the top N ranked keyphrases.
+
+        Args:
+            top_N_keyphrases (list[str]): A list of top N keyphrases.
+            references (list[str]): A list of reference keyphrases.
+            cutoff (int): The cutoff value for evaluation (default is 5).
+
+        Returns:
+            tuple[float, float, float]: The precision, recall, and F1-score.
+        """
         P = (
             len(set(top_N_keyphrases[:cutoff]) & set(references))
             / len(top_N_keyphrases[:cutoff])
@@ -75,20 +125,44 @@ class KpeEvalJob(ConfigurableJob):
         return (P, R, F)
 
     def run(self) -> None:
+        """Runs the keyphrase extraction evaluation job and saves the results as a json."""
+        data_dir = os.path.join(
+            "./data/eval_kpe/",
+            self.output_dir_name if self.output_dir_name else self.job_name,
+        )
+        os.makedirs(data_dir, exist_ok=True)
+
+        config = self.get_config()
+        logger.info(
+            f"Started keyphrase extraction evaluation job with this args:\n{config}"
+        )
+
         results = dict()
         for model in self.model_names_or_paths:
             name = model.split("/")[-1]
             results[name] = self.eval_model(model)
+        results["dataset"] = self.dataset
 
-        print(results)
-        print("=" * 10)
-        json_string = json.dumps(results, indent=4)
-        print(json_string)
+        with open(os.path.join(data_dir, "results.json"), "w") as f:
+            f.write(json.dumps(results, indent=4))
 
     @staticmethod
     def weight_candidates(
         text: str, preds: Any, offsets: Any, tensor: bool
     ) -> list[str]:
+        """At first it extracts possible candidates according to `get_candidate_pos`.
+        It computes the importance as a mazimum of the `preds` scores of the words in the phrase.
+        Then it reorders the canditates according to this importance.
+
+        Args:
+            text (str): The input text.
+            preds (Any): The model predictions.
+            offsets (Any): The offsets of the tokens.
+            tensor (bool): Whether the predictions are tensors.
+
+        Returns:
+            list[str]: A list of weighted candidate phrases.
+        """
         candidates = KpeEvalJob.get_candidate_pos(text)
         skipped = 0
         candidate_importance = []
@@ -102,21 +176,31 @@ class KpeEvalJob(ConfigurableJob):
                 skipped += 1
                 continue
             if tensor:
-                importance = preds[0, token_ids, 0].mean()
+                importance = preds[0, token_ids, 0].max()  # [not inserted, inserted]
             else:
-                importance = np.mean(preds[token_ids])
+                importance = np.max(preds[token_ids])
             candidate_importance.append((candidate.phrase, importance))
 
-        ranked_candidates = sorted(candidate_importance, key=lambda x: x[1])
+        ranked_candidates = sorted(
+            candidate_importance, key=lambda x: x[1], reverse=True
+        )
         return [e[0] for e in ranked_candidates]
 
     def load_dataset(self) -> tuple[list[str], list[str]]:
+        """Loads the dataset for evaluation. It loads only extractive labels.
+
+        Returns:
+            tuple[list[str], list[str]]: A tuple containing the test texts and their corresponding labels.
+        """
         if self.dataset in ["taln-ls2n/inspec", "taln-ls2n/semeval-2010-pre"]:
-            dataset = load_dataset(self.dataset, split=self.split)
-            dataset = dataset.select(range(10))
+            dataset = load_dataset(
+                self.dataset, split=self.split, trust_remote_code=True
+            )
+            # dataset = dataset.select(range(10))
 
             test_texts = [
-                (sample["title"] + ". " + sample["abstract"]) for sample in dataset  # type: ignore
+                (sample["title"] + ". " + sample["abstract"])  # type: ignore
+                for sample in dataset
             ]
             text_phrases = dataset["keyphrases"]  # type: ignore
             prmu = dataset["prmu"]  # type: ignore
@@ -131,7 +215,7 @@ class KpeEvalJob(ConfigurableJob):
             dataset = load_dataset(
                 self.dataset, split=self.split, name=self.dataset_subset
             )
-            dataset = dataset.select(range(10))
+            # dataset = dataset.select(range(10))
 
             test_texts = [" ".join(sample["document"]) for sample in dataset]  # type: ignore
             labels = dataset["extractive_keyphrases"]  # type: ignore
@@ -142,12 +226,21 @@ class KpeEvalJob(ConfigurableJob):
     def align_preds(
         predicted, labels: list[str]
     ) -> tuple[list[list[str]], list[list[str]]]:
+        """Aligns predicted keyphrases with reference keyphrases with stemming and casing.
+
+        Args:
+            predicted: The predicted keyphrases.
+            labels (list[str]): The reference keyphrases.
+
+        Returns:
+            tuple[list[list[str]], list[list[str]]]: The aligned model outputs and references.
+        """
         model_outputs = []
         references = []
         for i, prediction in enumerate(predicted):
             model_output = []
             for keyphrase in prediction:
-                toks = keyphrase.split()
+                toks = keyphrase.lower().split()
                 model_output.append(
                     " ".join([Stemmer("english").stem(tok) for tok in toks])
                 )
@@ -155,7 +248,7 @@ class KpeEvalJob(ConfigurableJob):
 
             reference = []
             for keyphrase in labels[i]:
-                toks = keyphrase.split()
+                toks = keyphrase.lower().split()
                 reference.append(
                     " ".join([Stemmer("english").stem(tok) for tok in toks])
                 )
@@ -167,10 +260,21 @@ class KpeEvalJob(ConfigurableJob):
     def eval_dict(
         model_outputs: list[list[str]], references: list[list[str]]
     ) -> dict[str, float]:
+        """Evaluates the model outputs and returns precision, recall, and F1-scores.
+
+        Args:
+            model_outputs (list[list[str]]): The model outputs.
+            references (list[list[str]]): The reference keyphrases.
+
+        Returns:
+            dict[str, float]: A dictionary containing the evaluation metrics.
+        """
         results = dict()
         for cutoff in [5, 10, 15]:
             scores = []
             for i, output in enumerate(model_outputs):
+                if len(references[i]) == 0:  # if no extractive keyphrase then skip
+                    continue
                 scores.append(KpeEvalJob.eval_prf(output, references[i], cutoff))
 
             P, R, F = np.mean(scores, axis=0)
@@ -185,6 +289,15 @@ class KpeEvalJob(ConfigurableJob):
     def tokenize(
         tokenizer: PreTrainedTokenizerBase, text: str
     ) -> tuple[BatchEncoding, torch.Tensor]:
+        """Tokenizes the input text using the provided tokenizer.
+
+        Args:
+            tokenizer (PreTrainedTokenizerBase): The tokenizer to use.
+            text (str): The input text.
+
+        Returns:
+            tuple[BatchEncoding, torch.Tensor]: The tokenized input and offsets.
+        """
         tokenized_inp = tokenizer(
             [text],
             truncation=True,
@@ -197,7 +310,7 @@ class KpeEvalJob(ConfigurableJob):
         )
         offsets = tokenized_inp.pop("offset_mapping")[0]
         if tokenized_inp["input_ids"].shape[0] > 1:  # type: ignore
-            raise ValueError("Too long inout text for the context size.")
+            raise ValueError("Too long input text for the context size.")
 
         return tokenized_inp, offsets
 
@@ -205,6 +318,14 @@ class KpeEvalJob(ConfigurableJob):
     def load_model_tokenizer(
         model_name_or_path,
     ) -> tuple[BertForTokenClassification | None, PreTrainedTokenizerBase]:
+        """Loads the model and tokenizer from the given path.
+
+        Args:
+            model_name_or_path: The path to the model or model name.
+
+        Returns:
+            tuple[BertForTokenClassification | None, PreTrainedTokenizerBase]: The loaded model and tokenizer.
+        """
         if model_name_or_path == "tf_idf":
             tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
             model = None
@@ -215,6 +336,14 @@ class KpeEvalJob(ConfigurableJob):
         return model, tokenizer
 
     def eval_model(self, model_name_or_path) -> dict[str, float]:
+        """Evaluates a model for keyphrase extraction.
+
+        Args:
+            model_name_or_path: The path to the model or model name.
+
+        Returns:
+            dict[str, float]: The evaluation metrics.
+        """
         model, tokenizer = KpeEvalJob.load_model_tokenizer(model_name_or_path)
 
         test_texts, labels = self.load_dataset()
